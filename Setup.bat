726fc68bfe561573d8d941455e681ce717aa1c64
Javier V <javilleg@ucm.es>
2024-04-30 11:44:02 +0200

    setup.bat arreglado

--- Setup.bat
+++ Setup.bat
@@ -1,10 +1,17 @@
 set MotorBin=.\LocoMotor\bin
 set Bin=.\bin
 
-call .\LocoMotor\CompilarLocoMotor.bat
+cd LocoMotor
+
+call .\CompilarLocoMotor.bat
+
+cd ..
 
 msbuild G8_JuegoDeSaltos.sln /p:configuration=Debug
 msbuild G8_JuegoDeSaltos.sln /p:configuration=Release
 
 xcopy %MotorBin% %Bin% /h /i /c /k /e /r /y
-start %Bin%\LocoMotor_Main.exe
+
+cd bin
+
+start LocoMotor_Main.exe
--- bin/Assets/Scenes/Menu.lua
+++ bin/Assets/Scenes/Menu.lua
@@ -1,7 +1,7 @@
 Menu = {
     MainCamera = {
         Camera = {
-            background = "0.9 0.7 0.5"
+            background = "0.2 0.4 0.7"
         },
         AudioListener = {},
         Transform = {
@@ -10,78 +10,30 @@ Menu = {
             rotation = "0 0 0"
         },
     },
-    MainMenuManager={
-        MainMenuManager={}
-    },
     TitleText = {
         UIText = {
-            anchor = "0.5 0.5",
-            position = "0 -250",
-            pivot = "0.5 0.5",
-            size = "50 50",
-            text = "Juego de Pistolas",
+            anchor = "0 0",
+            position = "60 60",
+            pivot = "0 0",
+            size = "50 70",
+            text = "G8: Juego de Saltos",
             font = "Heavitas",
-            alignment = "c",
-            colorTop = "0.9 0 0.2",
-            colorBot = "0.2 0 0.9"
+            alignment = "l",
+            colorTop = "0 0.9 0.2",
+            colorBot = "0 0.2 0.9"
         }
     },
     PlayText = {
         UIText = {
-            anchor = "0.5 0.5",
-            position = "0 -150",
-            pivot = "0.5 0.5",
-            size = "20 25",
-            text = "Pulsa Espacio o algun boton del mando para jugar",
+            anchor = "0 0",
+            position = "60 130",
+            pivot = "0 0",
+            size = "50 60",
+            text = "Pulsa 'Espacio' para empezar a jugar",
             font = "Heavitas",
-            alignment = "c",
-            colorTop = "0.9 0 0.2",
-            colorBot = "0.2 0 0.9"
+            alignment = "l",
+            colorTop = "0 0.9 0.2",
+            colorBot = "0 0.2 0.9"
         }
     },
-    -- CHARACTERS --
-    Player_01 = {
-        Transform = {
-            position = "-3 0 -15",
-            size = "1 1 1",
-            rotation = "0 0 0"
-        },
-        MeshRenderer = {
-            mesh = "CubemanMesh.mesh",
-            material = "CustomMaterial"
-        }
-    },
-    Player_02 = {
-        Transform = {
-            position = "3 0 -15",
-            size = "1 1 1",
-            rotation = "0 -40 0"
-        },
-        MeshRenderer = {
-            mesh = "ConemanMesh.mesh",
-            material = "RedCharacter"
-        }
-    },
-    Player_03 = {
-        Transform = {
-            position = "-8 0 -15",
-            size = "1 1 1",
-            rotation = "0 0 0"
-        },
-        MeshRenderer = {
-            mesh = "SpheremanMesh.mesh",
-            material = "GreenCharacter"
-        }
-    },
-    Player_04 = {
-        Transform = {
-            position = "8 0 -15",
-            size = "1 1 1",
-            rotation = "0 -40 0"
-        },
-        MeshRenderer = {
-            mesh = "CylindermanMesh.mesh",
-            material = "PurpleCharacter"
-        }
-    }
 }
